57b9cf7cf6700d5bd7315c2c8f78d536d0b320fe
Yuchen Lu <luyuchen.paul@gmail.com>
2019-10-14 22:53:45 -0400

    ignore csv

--- drift/lewis/gumbel.py
+++ drift/lewis/gumbel.py
@@ -8,7 +8,7 @@ from tensorboardX import SummaryWriter
 from torch.distributions import Categorical
 
 TRAIN_STEPS = 2500
-BATCH_SIZE = 100
+BATCH_SIZE = 500
 LOG_STEPS = 10
 LOG_NAME = 'log_gumbel'
 
@@ -34,7 +34,7 @@ def selfplay(speaker, listener, gumbel_temperature=0.1):
                 logstr.append("{}: {:.4f}".format(name, val))
                 writer.add_scalar(name, val, step)
             print(' '.join(logstr))
-            if stats['comm_acc'] > 0.99:
+            if stats['comm_acc'] > 0.95:
                 stats['step'] = step
                 return stats
 
