06b8d5adf73153701c33b3c9408a6c47c5ed772e
Yuchen Lu <luyuchen.paul@gmail.com>
2019-10-15 16:27:13 -0400

    Gumbel can also use GPU

--- drift/lewis/__init__.py
+++ drift/lewis/__init__.py
@@ -4,6 +4,10 @@ Some global settings
 import torch
 
 USE_GPU = torch.cuda.is_available()
+if USE_GPU:
+    print('Use GPU')
+else:
+    print('Use CPU')
 
 # Ratio of total objects used for evaluation [0,1]. Use all if we have GPU
 EVALUATION_RATIO = 0.005 if not USE_GPU else 1.
