d37d6ad55475cfdb0bb49991a5d3edc6342c5014
Yuchen Lu <luyuchen.paul@gmail.com>
2019-10-25 23:48:57 -0400

    Fix a bug in gumbel

--- drift/gumbel.py
+++ drift/gumbel.py
@@ -1,6 +1,7 @@
 from drift import USE_GPU
 import torch
 from torch.distributions import Categorical
+import torch.nn.functional as F
 
 BATCH_SIZE = 500
 
@@ -12,21 +13,25 @@ def selfplay_batch(game, gumbel_temperature, l_opt, listener, s_opt, speaker):
     if USE_GPU:
         objs = objs.cuda()
     s_logits = speaker(objs)
-    y = torch.nn.functional.softmax(s_logits / gumbel_temperature, dim=-1)
-    g = torch.distributions.Gumbel(loc=0, scale=1).sample(y.shape)
-    msgs = torch.argmax(torch.log(y) + g, dim=-1)
+    s_logprob = F.log_softmax(s_logits, dim=-1)
+    g = torch.distributions.Gumbel(loc=0, scale=1).sample(s_logits.shape)
+    y = F.softmax((g + s_logprob) / gumbel_temperature, dim=-1)
+    msgs = torch.argmax(y, dim=-1)
     # Get gradient to keep backprop to speaker
     oh_msgs = listener.one_hot(msgs)
     oh_msgs.requires_grad = True
     oh_msgs.grad = None
     l_logits = listener(oh_msgs)
+
     # Train listener
     l_logprobs = Categorical(logits=l_logits).log_prob(objs)
     l_logprobs = l_logprobs.sum(-1)
     l_opt.zero_grad()
     (-l_logprobs.mean()).backward(retain_graph=True)
     l_opt.step()
+
     # Train Speaker
     s_opt.zero_grad()
+    assert oh_msgs.grad is not None
     y.backward(oh_msgs.grad)
     s_opt.step()
